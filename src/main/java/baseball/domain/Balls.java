36cb61f765844ea58ed943cfdf5e3745ee8b8ee3
ssj5638 <ssj5638@gmail.com>
2021-10-04 16:22:48 +0900

    refactor

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import nextstep.utils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,11 +9,19 @@ public class Balls {
 
     private final List<Ball> comBalls;
 
-    public Balls(List<Integer> ballList) {
-        this.comBalls = ballMap(ballList);
+    public Balls() {
+        this.comBalls = makeComBall();
     }
 
-    private List<Ball> ballMap(List<Integer> ballList) {
+    private List<Ball> makeComBall() {
+        List<Ball> comBalls = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            comBalls.add(new Ball(i, Randoms.pickNumberInRange(1, 9)));
+        }
+        return comBalls;
+    }
+
+    private List<Ball> ballList(List<Integer> ballList) {
         List<Ball> ballMap = new ArrayList<>();
         Ball ball;
         for (int i = 0; i < ballList.size(); i++) {
@@ -24,7 +34,7 @@ public class Balls {
     // TODO : indent depth 줄이기
     public PlayResult play(List<Integer> ballList) {
         PlayResult playResult = new PlayResult();
-        List<Ball> userBalls = ballMap(ballList);
+        List<Ball> userBalls = ballList(ballList);
         for (Ball comball : comBalls) {
             for (Ball userBall : userBalls) {
                 playResult.report(comball.play(userBall));
